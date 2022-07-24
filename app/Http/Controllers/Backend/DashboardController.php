bd0ef241d6ca7a50b1e33f45f5ab27d4ed4de499
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-07-24 18:43:07 +0600

    dashboard permission

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -7,8 +7,20 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    protected $user;
+
+    public function __construct(){
+        $this->middleware(function($request, $next){
+            $this->user = Auth::guard('admin')->user();
+            return $next($request);
+        });
+    }
+
     public function index(){
 
+    if(is_null($this->user) || !$this->user->can('dashboard.view') ){
+        abort(403, 'Unauthonticated Access');
+    }
        return view("backends.pages.dashboad.index");
     }
 }
